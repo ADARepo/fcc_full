6891a4c5eb6951529f0dcd4484a82d4ab375580c
ADARepo <goddemraw@gmail.com>
2023-03-31 12:05:46 -0400

    properties untracked

--- src/main/java/com/adarepo/movies/MoviesApplication.java
+++ src/main/java/com/adarepo/movies/MoviesApplication.java
@@ -2,11 +2,15 @@ package com.adarepo.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MoviesApplication {
+public class MoviesApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
